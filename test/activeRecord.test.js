6304162819f059b3d758b9d70dbe3c3bac682a8c
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-23 10:43:52 -0500

    completed

---
+++ test/activeRecord.test.js
@@ -0,0 +1,79 @@
+'use strict';
+const { describe, it } = require( "mocha" );
+const ActiveRecord = require( '../lib/ActiveRecord' );
+const assert = require( "assert" );
+
+describe( "Active Record base class", () => {
+
+    it( "Contains findById", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.findById;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+
+    it( "Contains findOne", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.findOne;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+
+    it( "Contains findLastTen", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.findLastTen;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+
+    it( "Contains save", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.save;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+
+    it( "Contains update", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.update;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+
+    it( "Contains delete", done => {
+        // Arrange
+        const record = new ActiveRecord();
+
+        // Act
+        const typeOfProperty = typeof record.delete;
+
+        // Assert
+        assert( typeOfProperty === "function" );
+        done();
+    } );
+} );
